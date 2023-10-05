15ba52aac9c6ae4cb05b195dc6d9c3414d6090a1
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-10-05 16:41:43 -0300

    Initial commit

---
+++ src/test/java/Dio/Santanderdevwork2023/SantanderDevWork2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package Dio.Santanderdevwork2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWork2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
